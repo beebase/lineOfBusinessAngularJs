7b73f9758dd5fa008c68e55deeb30b5784074bb9
Maarten Berkenbosch <maarten@beebase.nl>
2015-03-23 13:53:06 +0100

    extend  ui.router with home and productEdit

--- app/app.js
+++ app/app.js
@@ -7,12 +7,21 @@
 
   app.config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
+      .state('home', {
+        url        : '/',
+        templateUrl: 'app/welcomeView.html'
+      })
       .state('productList', {
         url        : '/products',
         templateUrl: 'app/products/productListView.html',
         controller : 'ProductListCtrl as vm'
+      })
+      .state('productEdit', {
+        url        : 'app/products/edit/:productId',
+        templateUrl: 'app/products/productEditView.html',
+        controller : 'ProductEditCtrl as vm'
       });
-    $urlRouterProvider.otherwise("/products");
+    $urlRouterProvider.otherwise("/");
 
   });
 }());
